480ab71a5e363e7df7377984bed1b3bc454146da
hugowaldern <hwaldern@cca.edu>
2016-01-28 14:40:27 -0800

    Create Puzzles4.js

---
+++ Puzzles4.js
@@ -0,0 +1,21 @@
+down();
+down();
+down();
+down();
+right();
+if (getColor() == "red") {
+  getColor();
+}
+down();
+setColor("red");
+right();
+right();
+right();
+up();
+if (getColor() == "blue") {
+getColor();
+}
+down();
+setColor("blue");  
+right();
+right();
