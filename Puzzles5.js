fd0253bb7a26a8a9cbb72f71e14b231bbda79bd2
hugowaldern <hwaldern@cca.edu>
2016-01-28 14:41:00 -0800

    Create Puzzles5.js

---
+++ Puzzles5.js
@@ -0,0 +1,18 @@
+right();
+  right();
+while (getColor() == "blue") {
+  down();
+  right();
+  right();
+}
+while (getColor() == "red") {
+  up();
+  right();
+  right();
+}
+while (getColor() == "blue") {
+  down();
+  right();
+  right();
+}
+right();
